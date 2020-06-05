edfe5eff80078b27de011ddd92cc5cf54e262b63
georgep9 <g-patt@outlook.com>
2020-06-06 00:30:34 +1000

    added mysql database connection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const db = require('./database/db');
 var stocksRouter = require('./routes/stocks');
 var userRouter = require('./routes/user');
 
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(db);
 app.use('/stocks', stocksRouter);
 app.use('/user', userRouter);
 
