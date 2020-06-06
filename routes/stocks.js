344c05ecf720963571dfd02c688abff7b499f474
georgep9 <g-patt@outlook.com>
2020-06-06 23:07:19 +1000

    fully implemented /stocks/{symbol} endpoint

--- routes/stocks.js
+++ routes/stocks.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * /stocks/symbols API endpoint
+ */ 
 router.get('/symbols', function(req, res, next) {
 
     let query = req.db('stocks'); // get db query instance
@@ -38,12 +41,48 @@ router.get('/symbols', function(req, res, next) {
   
 });
 
-
+/**
+ * /stocks/{symbol} API endpoint
+ */
 router.get('/:symbol', function(req, res, next) {
-  res.render('index', { title: "Symbol" });
+
+  let query = req.db('stocks'); // get db query instance
+
+  // check if symbol is correct format
+  if (/^[A-Z]{1,5}$/.test(req.params.symbol)){
+
+      // filter query for symbol provided
+      query.where("symbol", "=", req.params.symbol);
+
+      query.distinct("*")
+        .then((rows) => {
+          // if there are rows, respond with stock object json
+          if (rows.length){
+            res.status(200);
+            res.json(rows[0]); // send only latest time entry
+          }
+          else { // no rows return, respond with error
+            res.status(404);
+            res.json({ error: true, message: "No entry for symbol in stocks database"})
+          }
+        })
+
+  }
+  else { // incorrect format, respond with error
+    res.status(400);
+    res.json({error: true, mesage: "Stock symbol incorrect format - must be 1-5 capital letters"});
+  }
+
 });
 
+
+/**
+ * /stocks/authed/{symbol} API endpoint
+ */
 router.get('/authed', function(req, res, next) {
+
+  let query = req.db('stocks'); // get db query instance
+
   res.render('index', { title: 'Authed' });
 });
 
