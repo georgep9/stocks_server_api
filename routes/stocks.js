ee1d156fb85be51c724dbd9c75ba35dcdd7f575b
georgep9 <g-patt@outlook.com>
2020-06-06 22:04:04 +1000

    implemented 'Invalid query parameter' response for /stocks/symbols/

--- routes/stocks.js
+++ routes/stocks.js
@@ -3,23 +3,37 @@ var router = express.Router();
 
 router.get('/symbols', function(req, res, next) {
 
-    let query = req.db('stocks');
+    let query = req.db('stocks'); // get db query instance
 
-    req.query.industry && query.where("industry", "like", `%${req.query.industry}%`);
+    // check if industry queried
+    if (req.query.industry){
+      // if valid format, filter db query for industry provided
+      if (/^[a-zA-Z]+$/.test(req.query.industry)){
+        query.where("industry", "like", `%${req.query.industry}%`);
+      }
+      else { // else, respond with error
+        res.status(400);
+        res.json({error: true, message: "Invalid query parameter: only 'industry' is permitted"});
+        return;
+      }
+    }
 
+    // distinct select name, symbol, industry columns from db
     query.distinct("name", "symbol", "industry")
       .then((rows) => {
+        // if there are rows, repond with json of rows
         if (rows.length){
           res.status(200);
           res.json(rows);
-        } else {
-          res.json({ error: true, Message: "Industry sector not found" })
+        } 
+        else { // no rows returned, respond with error
+          res.status(404);
+          res.json({ error: true, message: "Industry sector not found" })
         }
-        
       })
-      .catch((err) => {
+      .catch((err) => { // sql error
         console.log(err)
-        res.json({ Error: false, Message: "Error in MySQL query" })
+        res.json({ error: true, message: "Error in MySQL query" })
       })
   
 });
