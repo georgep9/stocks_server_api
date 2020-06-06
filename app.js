eb67143d439fa377920d4494745d577d03c8644e
georgep9 <g-patt@outlook.com>
2020-06-06 21:42:33 +1000

    added industry querying for /stocks/symbols

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("dotenv").config();
 
 const knexOptions = require('./knexfile.js');
 const knex = require('knex')(knexOptions);
@@ -11,7 +12,8 @@ const swaggerUI = require('swagger-ui-express');
 const yaml = require('yamljs');
 const swaggerDocument = yaml.load('./swagger.yaml');
 
-
+const helmet = require('helmet');
+const cors = require('cors');
 
 var stocksRouter = require('./routes/stocks');
 var userRouter = require('./routes/user');
@@ -23,6 +25,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('common'));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/stocks.js
+++ routes/stocks.js
@@ -2,15 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/symbols', function(req, res, next) {
-	req.db.from('stocks').distinct(['name', 'symbol', 'industry'])
-		.then ((rows) => {
-			res.json(rows)
-		})
-		.catch((err) => {
-			console.log(err);
-			res.json({"Error": true, "Message": "Error executing MySQL query"})
-		})
+
+    let query = req.db('stocks');
+
+    req.query.industry && query.where("industry", "like", `%${req.query.industry}%`);
+
+    query.distinct("name", "symbol", "industry")
+      .then((rows) => {
+        if (rows.length){
+          res.status(200);
+          res.json(rows);
+        } else {
+          res.json({ error: true, Message: "Industry sector not found" })
+        }
+        
+      })
+      .catch((err) => {
+        console.log(err)
+        res.json({ Error: false, Message: "Error in MySQL query" })
+      })
+  
 });
+
 
 router.get('/:symbol', function(req, res, next) {
   res.render('index', { title: "Symbol" });
